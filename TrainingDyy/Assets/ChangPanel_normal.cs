75ce4b6769b43012c6599f1e0686c9286eefe05c
mmmable <1105571366@qq.com>
2018-09-15 10:35:35 +0800

    0915am按钮交互

---
+++ TrainingDyy/Assets/ChangPanel_normal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangPanel_normal : MonoBehaviour {
+
+    
+    
+        public GameObject next;
+        public GameObject back;
+        public GameObject present;
+
+        public void Next()
+        {
+            present.SetActive(false);
+            next.SetActive(true);
+        }
+        public void Back()
+        {
+            present.SetActive(false);
+            back.SetActive(true);
+
+        }
+    }
---
+++ TrainingDyy/Assets/Changepanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changepanel : MonoBehaviour {
+    public GameObject next;
+   
+    public GameObject present;
+
+	public void Next() {
+        present.SetActive(false);
+            next.SetActive(true);
+	}
+    
+
+
+
+
+
+
+}
