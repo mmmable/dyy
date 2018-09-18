d07225a3c27d28af54cf4d8313d34e15f6d88f83
pann5 <pann5@mail2.sysu.edu.cn>
2018-09-18 12:20:34 +0800

    text

--- TrainingDyy/Assets/Selections.cs
+++ TrainingDyy/Assets/Selections.cs
@@ -16,37 +16,37 @@ public class Selections : MonoBehaviour {
 
     public void TocolorA()
     {
-        transform.GetChild(0).GetComponent<MeshRenderer>().materials[1].color = Color.red; //transform.GetComponent<MeshRenderer>().materials[1].color = Color.red;颜色变成红色
+        transform.GetComponent<MeshRenderer>().materials[0].color = Color.red; //transform.GetComponent<MeshRenderer>().materials[1].color = Color.red;颜色变成红色
         AudioSource.PlayClipAtPoint(clip, transform.position); //音效
     }
 
     public void TocolorB()
     {
-        transform.GetChild(0).GetComponent<MeshRenderer>().materials[1].color = Color.blue;
+        transform.GetComponent<MeshRenderer>().materials[0].color = Color.blue;
         AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 
     public void TocolorC()
     {
-        transform.GetChild(0).GetComponent<MeshRenderer>().materials[1].color = Color.green;
+        transform.GetComponent<MeshRenderer>().materials[0].color = Color.green;
         AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 
     public void ToS()
     {
-        transform.localScale = new Vector3(0.5F, 0.5F, 0.5F); //transform.localScale = new Vector3(0.5F, 0.5F, 0.5F)
+        transform.localScale = new Vector3(0.1F, 0.1F, 0.1F); //transform.localScale = new Vector3(0.5F, 0.5F, 0.5F)
         AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 
     public void ToM()
     {
-        transform.localScale = new Vector3(1, 1, 1);
+        transform.localScale = new Vector3(0.25F, 0.25F, 0.25F);
         AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 
     public void ToL()
     {
-        transform.localScale = new Vector3(1.5F, 1.5F, 1.5F);
+        transform.localScale = new Vector3(0.5F, 0.5F, 0.5F);
         AudioSource.PlayClipAtPoint(clip, transform.position);
     }
 }
