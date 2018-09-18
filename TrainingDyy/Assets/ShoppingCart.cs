997b8e07b803e06738b96af96459c064858e6bd6
pann5 <pann5@mail2.sysu.edu.cn>
2018-09-18 21:49:23 +0800

    cart

--- TrainingDyy/Assets/ShoppingCart.cs
+++ TrainingDyy/Assets/ShoppingCart.cs
@@ -9,12 +9,9 @@ public class ShoppingCart : MonoBehaviour {
     private Vector3 pointCart;
     private Vector3 pointMove;
     public GameObject Cart;
-    public Material red;
+    
+    //public AudioClip clip;
 
-    public AudioClip clip;
-
-    public float bigScale;
-    public float smallScale;
 
     void Start () {
         pointZero = transform.position;
@@ -24,25 +21,13 @@ public class ShoppingCart : MonoBehaviour {
         pointMove = gameObject.transform.position;           //实时获取物体的位置
         pointCart = Cart.GetComponent<Transform>().position; //实时获取购物车的位置
 
-        if (Vector3.Distance(transform.position, pointCart) < 0.5) //判断以上两个位置之间的距离
-        {
-            Cart.transform.localScale = new Vector3(bigScale, bigScale, bigScale); //购物车放大
-            Cart.GetComponent<MeshRenderer>().materials[1] = red;
-        }
 
-        if (Vector3.Distance(transform.position, pointCart) < 0.05)
+        if (Vector3.Distance(transform.position, pointCart) < 0.1)
         {
-            Cart.GetComponent<MeshRenderer>().materials[1].SetColor("_TintColor", Color.red);
+            Cart.GetComponent<MeshRenderer>().materials[0].color = Color.black;
             Destroy(gameObject);                                     //销毁物体
-            AudioSource.PlayClipAtPoint(clip, transform.position);   //音效
+            //AudioSource.PlayClipAtPoint(clip, transform.position);   //音效
         }
 
-
-
-        if (Vector3.Distance(transform.position, pointCart) > 0.5)
-        {
-            Cart.transform.localScale = new Vector3(smallScale, smallScale, smallScale); //距离远，购物车缩小
-            Cart.GetComponent<MeshRenderer>().materials[1] = red;
-        }
     }
 }
