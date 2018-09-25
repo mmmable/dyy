723fb2f94353113e1f25694e0c16f788ff340168
mmmable <1105571366@qq.com>
2018-09-25 20:11:36 +0800

    timer test1

---
+++ TrainingDyy/Assets/MoveCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+
+    public GameObject c1;
+    public Vector3 a;
+   // public targetPosition:Vector3=Vector3(10,0,0);//指定位置
+
+    // Use this for initialization
+    void Start () {
+       // c1.SetActive(true);
+        //a = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+    }
+
+    public void Move()
+    {
+        // transform.position = Vector3.MoveTowards(transform.position, a, Time.deltaTime * 2);//移动到指定位置
+        c1.SetActive(true);
+        c1.transform.position = a;
+    }
+
+
+    
+    
+}
+
---
+++ TrainingDyy/Assets/Timer.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private int currentIndex = 0;
+    private float currentTimer = 0f;
+    bool isTimer = false;
+
+    private Dictionary<int, float> timerDictionary = new Dictionary<int, float>();
+    //private Dictionary<int,string> nameDictionary = new Dictionary<int, string> ();
+
+
+
+    //public Text ShowTime;//显示计时器时间
+    /*
+    public float t0;
+    public float t1;
+    public float t2;
+    public float t3;
+    public float t4;
+    public float t5;
+    public float t6;
+    public float t7;
+    public float t8;
+    public float t9;
+ */
+
+    // Use this for initialization
+    void Start()
+    {
+        timerDictionary.Add(0, 0f);
+        timerDictionary.Add(1, 0f);
+        timerDictionary.Add(2, 0f);
+        timerDictionary.Add(3, 0f);
+        timerDictionary.Add(4, 0f);
+        timerDictionary.Add(5, 0f);
+        timerDictionary.Add(6, 0f);
+        timerDictionary.Add(7, 0f);
+        timerDictionary.Add(8, 0f);
+        timerDictionary.Add(9, 0f);
+
+
+        EnterPlane(0);
+
+        /*
+		JsonObject jo = // ************************ /
+		string textPath = jo.GetString ("TextPath"); 
+		JsonObject testObj = jo.GetObj("testObj");
+		JsonArray testArray = jo.GetArray("testArray");
+		//------
+
+		//EnCode
+		JsonObject newObj = new JsonObject ();
+		newObj.SetString("TextPath",str);
+		testObj = new JsonObj();
+		newObj.SetObj("testObj",testObj);
+		newObj.SetArray("testArray",testArray);
+
+		string bigObjStr = newObj.ToString();
+		string path = "D:/test/bigObjStr.json";
+		FileManager.SaveToString(path,bigObjStr);
+		*/
+
+        //NextPlane ();
+        //ShowTime = GetComponent<Text>();//显示计时器时间
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //currentTimer += Time.deltaTime;
+        if (isTimer)
+        {
+            timerDictionary[currentIndex] += Time.deltaTime;
+
+            //ShowTime.text = currentTimer.ToString("F2");//显示计时器时间
+
+        }
+    }
+
+    public void EnterPlane(int index)
+    {
+        if (index < timerDictionary.Count && index >= 0)
+        {
+            isTimer = true;
+            currentIndex = index;
+        }
+        else
+        {
+            isTimer = false;
+        }
+    }
+
+    public void BackPlane()
+    {
+        EnterPlane(currentIndex - 1);
+    }
+
+    public void NextPlane()
+    {
+        EnterPlane(currentIndex + 1);
+    }
+
+    public void OnApplicationQuit()
+    {
+        Debug.Log("This is the end of game!");
+        foreach (var item in timerDictionary)
+        {
+            Debug.Log("This is the " + item.Key + "Plane");
+            Debug.Log("and the Time count is" + item.Value + "s.");
+        }
+
+    }
+
+}
